2d1e78de19533af05301b4380f9d34908627b2cf
nabal <nabal@DESKTOP-6CE3VQU.gateway>
2019-05-18 17:05:50 +1000

    Overridden the getDetails method

--- s3541341_A2/src/cars/SilverServiceCar.java
+++ s3541341_A2/src/cars/SilverServiceCar.java
@@ -1,48 +1,36 @@
 package cars;
 
 import utilities.DateTime;
+import utilities.DateUtilities;
 
 public class SilverServiceCar extends Car {
 	// Defining new variables
 	private double bookingFee;
 	private String[] refreshments;
-	private double tripFee = bookingFee * 0.4; 
-	
-	//Adding constructor for this new class
-	public SilverServiceCar(String regNo, String make, String model, String driverName, 
-			int passengerCapacity, double bookingFee, String[] refreshments)
-	{
-		//Super: Calling the previous variables for use in this class
+	private double tripFee = bookingFee * 0.4;
+
+	// Adding constructor for this new class
+	public SilverServiceCar(String regNo, String make, String model, String driverName, int passengerCapacity,
+			double bookingFee, String[] refreshments) {
+		// Super: Calling the previous variables for use in this class
 		super(regNo, make, model, driverName, passengerCapacity);
-		
-		//Booking Fee must be greater than or equal to $3.00
+		// Booking Fee must be greater than or equal to $3.00
 		if (bookingFee >= 3.0) {
 			this.bookingFee = bookingFee;
 		}
 	}
-	
-	/*@Override
-	public boolean book(String firstName, String lastName, DateTime required, int numPassengers)
-	{
-		/*SUPERCLASS: BOOK METHOD***
-		 * boolean booked = false;
-		// Does car have five bookings
-		available = bookingAvailable();
-		boolean dateAvailable = notCurrentlyBookedOnDate(required);
-		// Date is within range, not in past and within the next week
-		boolean dateValid = dateIsValid(required);
-		// Number of passengers does not exceed the passenger capacity and is not zero.
-		boolean validPassengerNumber = numberOfPassengersIsValid(numPassengers);
 
-		// Booking is permissible
-		if (available && dateAvailable && dateValid && validPassengerNumber)
-		{
-			tripFee = STANDARD_BOOKING_FEE;
-			Booking booking = new Booking(firstName, lastName, required, numPassengers, this);
-			currentBookings[bookingSpotAvailable] = booking;
-			bookingSpotAvailable++;
-			booked = true;
+	// Overriding the Book method so that bookings can only be made 3 days in advance.
+
+	// Overriding the getDetails method to display current bookings and refreshments.
+	public String getDetails() {
+		String overString = "";
+		overString = super.getDetails();
+		overString += ("\nBooking Fee: " + bookingFee + "\nRefreshments Available:\n"); 
+		for (int i = 0; i < refreshments.length; i++) {
+			overString += ("\n" + refreshments[i]);
 		}
-		return booked;
-	}*/
+		overString += "\n\n";
+		return overString;
+	}
 }
